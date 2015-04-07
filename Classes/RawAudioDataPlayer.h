5557e2f82496f7fc5b228df6c8ee6e62a41a7fb7
YIN GONG <max.g.laboratory@gmail.com>
2015-04-07 11:28:36 +0930

    minor Changes
    Minor Changes， the record function is ok, need to transfer to server.

--- Classes/RawAudioDataPlayer.h
+++ Classes/RawAudioDataPlayer.h
@@ -2,7 +2,6 @@
 //  RawAudioDataPlayer.h
 //  OggSpeex
 //
-//  Created by Jiang Chuncheng on 6/25/13.
 //  Copyright (c) 2013 Sense Force. All rights reserved.
 //
 
--- ViewController.h
+++ ViewController.h
@@ -7,26 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AsyncUdpSocket.h"
+
 
 @class CL_AudioRecorder;
 
-#define kRecorderDirectory [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]  stringByAppendingPathComponent:@"Recorders"]
-
 
 @interface ViewController : UIViewController
-{
-    AsyncUdpSocket   *m_udpSocket;
-    int              m_tcpSocket;
 
-}
-
-@property (strong, nonatomic) IBOutlet UIWebView *MyUIWeb;
-
-@property (strong, nonatomic) IBOutlet UIButton *udpSendBtn;
-
-- (IBAction)udpSendAction:(id)sender;
-
+@property (strong, nonatomic) IBOutlet UIWebView *MyWebUi;
 
 
 
